299c3b3641b3b254293fc46934ecc0bd9549fbf1
TakuKarasawa <taku-ka@outlook.com>
2022-12-03 14:08:25 +0900

    add inpaintor

--- include/utils/inpaintor.h
+++ include/utils/inpaintor.h
@@ -1,16 +1,47 @@
 #ifndef INPAINTOR_H_
 #define INPAINTOR_H_
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/fuzzy.hpp>
+
 namespace place_recognition
 {
 class Inpaintor
 {
 public:
-	Inpaintor();
+	Inpaintor() { }
+
+	void inpaint_img(cv::Mat input_img,cv::Mat& output_img)
+	{
+		// resize
+		resize_img(input_img);
+
+		// mask img
+		cv::Mat mask_img = cv::Mat::zeros(input_img.size(),CV_8UC3);
+		cv::rectangle(mask_img,cv::Point(150,0),cv::Point(210,input_img.rows),cv::Scalar(255,255,255),-1,cv::LINE_AA);
+		cv::rectangle(mask_img,cv::Point(420,0),cv::Point(490,input_img.rows),cv::Scalar(255,255,255),-1,cv::LINE_AA);
+		cv::rectangle(mask_img,cv::Point(800,0),cv::Point(840,input_img.rows),cv::Scalar(255,255,255),-1,cv::LINE_AA);
+		cv::rectangle(mask_img,cv::Point(1060,0),cv::Point(1120,input_img.rows),cv::Scalar(255,255,255),-1,cv::LINE_AA);	
+		cv::cvtColor(mask_img,mask_img,cv::COLOR_BGR2GRAY);
+
+		// inpaint img
+		cv::Mat inpaint_img;
+		cv::inpaint(input_img,mask_img,inpaint_img,3,cv::INPAINT_TELEA);
+		output_img = inpaint_img;		
+	}
+
+	void resize_img(cv::Mat& img)
+	{
+		// int col_size = img.cols;
+		// int row_size = img.rows;
+		cv::Rect rect(cv::Point(0,265),cv::Size(img.cols,235));
+		img = img(rect);
+	}
 
 private:
-
-
 };
 }
 
--- src/keypoints_detector.cpp
+++ src/keypoints_detector.cpp
@@ -1,6 +1,11 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
 
+#include "utils/inpaintor.h"
+
+namespace place_recognition
+{
 class KeypointsDetector
 {
 public:
@@ -8,14 +13,54 @@ public:
 	void process();
 
 private:
+	void image_callback(const sensor_msgs::ImageConstPtr& msg);
+
+	// node handler
 	ros::NodeHandle nh_;
 	ros::NodeHandle private_nh_;
 
+	// subscriber
+	ros::Subscriber img_sub_;
+
+	// inpaintor
+	Inpaintor* inpaintor_;
 };
+}
+
+using namespace place_recognition;
 
 KeypointsDetector::KeypointsDetector() :
-	private_nh_("~")
+	private_nh_("~"),
+	inpaintor_(new Inpaintor)
 {
+	img_sub_ = nh_.subscribe("img_in",1,&KeypointsDetector::image_callback,this);
+}
+
+void KeypointsDetector::image_callback(const sensor_msgs::ImageConstPtr& msg)
+{
+	cv_bridge::CvImagePtr cv_ptr;
+    try{
+        cv_ptr = cv_bridge::toCvCopy(msg,sensor_msgs::image_encodings::BGR8);
+    }
+    catch(cv_bridge::Exception& ex){
+        ROS_ERROR("Could not convert to color image");
+        return;
+    }
+
+	cv::Mat resized_img(cv_ptr->image.rows,cv_ptr->image.cols,cv_ptr->image.type());
+	resized_img = cv_ptr->image;
+
+	cv::Mat inpainted_img(cv_ptr->image.rows,cv_ptr->image.cols,cv_ptr->image.type());
+	inpainted_img = cv_ptr->image;
+
+	inpaintor_->resize_img(resized_img);
+	inpaintor_->inpaint_img(cv_ptr->image,inpainted_img);
+
+	std::cout << "Resize  Img: (" << resized_img.cols << "," << resized_img.rows << ")" << std::endl;
+	std::cout << "Inpaint Img: (" << inpainted_img.cols << "," << inpainted_img.rows << ")" << std::endl;
+	std::cout << std::endl;
+
+	// TO DO (convine)
 
 }
 
